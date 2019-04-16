4f666e134256ea4dd3f9b3022a588d7a3bda10c9
trevholm <trevholm@iu.edu>
2019-04-16 15:09:54 -0400

    commit

--- base/Test.java
+++ base/Test.java
@@ -10,24 +10,24 @@ class Test{
 		char ar[][] = {{'r','k','b','K','Q','b','k','r'},{'p','p','p','p','p','p','p','p'},{' ',' ',' ',' ',' ',' ',' ',' '},{' ',' ',' ',' ',' ',' ',' ',' '},
 						{' ',' ',' ',' ',' ',' ',' ',' '},{' ',' ',' ',' ',' ',' ',' ',' '},{'p','p','p','p','p','p','p','p'},{'r','k','b','K','Q','b','k','r'}};
 		
-		System.out.println("---------------------------------");
-		System.out.printf("| %c | %c | %c | %c | %c | %c | %c | %c |\n", ar[7][0], ar[7][1], ar[7][2], ar[7][3], ar[7][4], ar[7][5], ar[7][6], ar[7][7]);
-		System.out.println("|---|---|---|---|---|---|---|---|");
-		System.out.printf("| %c | %c | %c | %c | %c | %c | %c | %c |\n", ar[6][0], ar[6][1], ar[6][2], ar[6][3], ar[6][4], ar[6][5], ar[6][6], ar[6][7]);
-		System.out.println("|---|---|---|---|---|---|---|---|");
-		System.out.printf("| %c | %c | %c | %c | %c | %c | %c | %c |\n", ar[5][0], ar[5][1], ar[5][2], ar[5][3], ar[5][4], ar[5][5], ar[5][6], ar[5][7]);
-		System.out.println("|---|---|---|---|---|---|---|---|");
-		System.out.printf("| %c | %c | %c | %c | %c | %c | %c | %c |\n", ar[4][0], ar[4][1], ar[4][2], ar[4][3], ar[4][4], ar[4][5], ar[4][6], ar[4][7]);
-		System.out.println("|---|---|---|---|---|---|---|---|");
-		System.out.printf("| %c | %c | %c | %c | %c | %c | %c | %c |\n", ar[3][0], ar[3][1],ar[3][2], ar[3][3], ar[3][4], ar[3][5], ar[3][6], ar[3][7]);
-		System.out.println("|---|---|---|---|---|---|---|---|");
-		System.out.printf("| %c | %c | %c | %c | %c | %c | %c | %c |\n", ar[2][0], ar[2][1], ar[2][2], ar[2][3], ar[2][4], ar[2][5], ar[2][6], ar[2][7]);
-		System.out.println("|---|---|---|---|---|---|---|---|");
-		System.out.printf("| %c | %c | %c | %c | %c | %c | %c | %c |\n", ar[1][0], ar[1][1], ar[1][2], ar[1][3], ar[1][4], ar[1][5], ar[1][6], ar[1][7]);
-		System.out.println("|---|---|---|---|---|---|---|---|");
-		System.out.printf("| %c | %c | %c | %c | %c | %c | %c | %c |\n", ar[0][0], ar[0][1], ar[0][2], ar[0][3], ar[0][4], ar[0][5], ar[0][6], ar[0][7]);
-		System.out.println("---------------------------------");
-
+		System.out.println(" ---------------------------------");
+		System.out.printf("8| %c | %c | %c | %c | %c | %c | %c | %c |\n", ar[7][0], ar[7][1], ar[7][2], ar[7][3], ar[7][4], ar[7][5], ar[7][6], ar[7][7]);
+		System.out.println(" |---|---|---|---|---|---|---|---|");
+		System.out.printf("7| %c | %c | %c | %c | %c | %c | %c | %c |\n", ar[6][0], ar[6][1], ar[6][2], ar[6][3], ar[6][4], ar[6][5], ar[6][6], ar[6][7]);
+		System.out.println(" |---|---|---|---|---|---|---|---|");
+		System.out.printf("6| %c | %c | %c | %c | %c | %c | %c | %c |\n", ar[5][0], ar[5][1], ar[5][2], ar[5][3], ar[5][4], ar[5][5], ar[5][6], ar[5][7]);
+		System.out.println(" |---|---|---|---|---|---|---|---|");
+		System.out.printf("5| %c | %c | %c | %c | %c | %c | %c | %c |\n", ar[4][0], ar[4][1], ar[4][2], ar[4][3], ar[4][4], ar[4][5], ar[4][6], ar[4][7]);
+		System.out.println(" |---|---|---|---|---|---|---|---|");
+		System.out.printf("4| %c | %c | %c | %c | %c | %c | %c | %c |\n", ar[3][0], ar[3][1],ar[3][2], ar[3][3], ar[3][4], ar[3][5], ar[3][6], ar[3][7]);
+		System.out.println(" |---|---|---|---|---|---|---|---|");
+		System.out.printf("3| %c | %c | %c | %c | %c | %c | %c | %c |\n", ar[2][0], ar[2][1], ar[2][2], ar[2][3], ar[2][4], ar[2][5], ar[2][6], ar[2][7]);
+		System.out.println(" |---|---|---|---|---|---|---|---|");
+		System.out.printf("2| %c | %c | %c | %c | %c | %c | %c | %c |\n", ar[1][0], ar[1][1], ar[1][2], ar[1][3], ar[1][4], ar[1][5], ar[1][6], ar[1][7]);
+		System.out.println(" |---|---|---|---|---|---|---|---|");
+		System.out.printf("1| %c | %c | %c | %c | %c | %c | %c | %c |\n", ar[0][0], ar[0][1], ar[0][2], ar[0][3], ar[0][4], ar[0][5], ar[0][6], ar[0][7]);
+		System.out.println(" ---------------------------------");
+		System.out.println("   A   B   C   D   E   F   G   H  ");
 	}//end constructor
 
 

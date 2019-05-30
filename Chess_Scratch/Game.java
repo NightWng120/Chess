0eff5049eb03b76a978a05958c77bd13dd3c2fce
NightWng120 <treverholmes9@gmail.com>
2019-05-30 19:34:24 -0400

    Update Game.java

--- Chess_Scratch/Game.java
+++ Chess_Scratch/Game.java
@@ -11,7 +11,7 @@ class Game{
 	
 	public Game(){
 		this.rook = new Rook();
-		this.rook.setColor(false);	
+		this.rook.setColor(false);
 		prePosfill(rook);
 		String ar[][] = {{" "," "," "," "," "," "," "," "},{" "," "," "," "," "," "," "," "},{" "," "," "," "," "," "," "," "},{" "," "," "," "," "," "," "," "},
 				{" "," "," "," "," "," "," "," "},{" "," "," "," "," "," "," "," "},{" "," "," "," "," "," "," "," "},{" "," "," "," "," "," "," "," "}};;
@@ -30,7 +30,9 @@ class Game{
 		userin = input.nextLine();			
 		
 		while(loop){
+			
 			posFill(ar);
+			print();
 			System.out.println("|-------------------------|");
 			System.out.println("| Enter the Coords of your|");			
 			System.out.println("| next move               |");			
@@ -60,7 +62,16 @@ class Game{
 				System.out.println("***Invalid Move***");
 				System.out.printf("\n\n\n");
 
-				System.out.println(" ---------------------------------");
+				print();
+				continue;
+			}//end else if
+
+		}//end while loop
+		
+	}//end constructor
+	
+	public void print(){
+		System.out.println(" ---------------------------------");
 				System.out.printf("7| %s | %s | %s | %s | %s | %s | %s | %s |\n", ar[0][7], ar[1][7], ar[2][7], ar[3][7], ar[4][7], ar[5][7], ar[6][7], ar[7][7]);
 				System.out.println(" |---|---|---|---|---|---|---|---|");
 				System.out.printf("6| %s | %s | %s | %s | %s | %s | %s | %s |\n", ar[0][6], ar[1][6], ar[2][6], ar[3][6], ar[4][6], ar[5][6], ar[6][6], ar[7][6]);
@@ -79,31 +90,8 @@ class Game{
 				System.out.println(" ---------------------------------");
 				System.out.println("   0   1   2   3   4   5   6   7  ");
 				System.out.printf("\n\n");
-				continue;
-			}//end else if
-
-			System.out.println(" ---------------------------------");
-			System.out.printf("7| %s | %s | %s | %s | %s | %s | %s | %s |\n", ar[0][7], ar[1][7], ar[2][7], ar[3][7], ar[4][7], ar[5][7], ar[6][7], ar[7][7]);
-			System.out.println(" |---|---|---|---|---|---|---|---|");
-			System.out.printf("6| %s | %s | %s | %s | %s | %s | %s | %s |\n", ar[0][6], ar[1][6], ar[2][6], ar[3][6], ar[4][6], ar[5][6], ar[6][6], ar[7][6]);
-			System.out.println(" |---|---|---|---|---|---|---|---|");
-			System.out.printf("5| %s | %s | %s | %s | %s | %s | %s | %s |\n", ar[0][5], ar[1][5], ar[2][5], ar[3][5], ar[4][5], ar[5][5], ar[6][5], ar[7][5]);
-			System.out.println(" |---|---|---|---|---|---|---|---|");
-			System.out.printf("4| %s | %s | %s | %s | %s | %s | %s | %s |\n", ar[0][4], ar[1][4], ar[2][4], ar[3][4], ar[4][4], ar[5][4], ar[6][4], ar[7][4]);
-			System.out.println(" |---|---|---|---|---|---|---|---|");
-			System.out.printf("3| %s | %s | %s | %s | %s | %s | %s | %s |\n", ar[0][3], ar[1][3],ar[2][3], ar[3][3], ar[4][3], ar[5][3], ar[6][3], ar[7][3]);
-			System.out.println(" |---|---|---|---|---|---|---|---|");
-			System.out.printf("2| %s | %s | %s | %s | %s | %s | %s | %s |\n", ar[0][2], ar[1][2], ar[2][2], ar[3][2], ar[4][2], ar[5][2], ar[6][2], ar[7][2]);
-			System.out.println(" |---|---|---|---|---|---|---|---|");
-			System.out.printf("1| %s | %s | %s | %s | %s | %s | %s | %s |\n", ar[0][1], ar[1][1], ar[2][1], ar[3][1], ar[4][1], ar[5][1], ar[6][1], ar[7][1]);
-			System.out.println(" |---|---|---|---|---|---|---|---|");
-			System.out.printf("0| %s | %s | %s | %s | %s | %s | %s | %s |\n", ar[0][0], ar[1][0], ar[2][0], ar[3][0], ar[4][0], ar[5][0], ar[6][0], ar[7][0]);
-			System.out.println(" ---------------------------------");
-			System.out.println("   0   1   2   3   4   5   6   7  ");
-			System.out.printf("\n\n");
-		}//end while loop
 		
-	}//end constructor
+	}//end print
 	
 	public void prePosfill(Rook rook){
 		int[] ar[][] = {{{0,0},{1,0},{2,0},{3,0},{4,0},{5,0},{6,0},{7,0}},{{0,1},{1,1},{2,1},{3,1},{4,1},{5,1},{6,1},{7,1}},{{0,2},{1,2},{2,2},{3,2},{4,2},{5,2},{6,2},{7,2}}

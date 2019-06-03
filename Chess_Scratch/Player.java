0e5a4ab8309c6e647e00548c7e6a048efc414b42
NightWng120 <treverholmes9@gmail.com>
2019-06-03 00:07:58 -0400

    weird shadowing

--- Chess_Scratch/Player.java
+++ Chess_Scratch/Player.java
@@ -1,6 +1,7 @@
 import java.util.*;
 public abstract class Player{
-
+	
+	protected Pawn pawn;
 	protected Rook rook;
 	protected Knight knight;
 	protected boolean color;
@@ -18,15 +19,17 @@ public abstract class Player{
 		this.color = color;
 		this.rook = new Rook(this.color);
 		this.knight = new Knight(this.color);
-		
+		this.pawn = new Pawn(this.color);
 	}//end constructor
 		
 	public boolean MoveCheck(int[] pos, int[] next){
 		int[] arK = this.knight.getPos();
 		int[] arR = this.rook.getPos();
+		int[] arP = this.pawn.getPos();
+		
 		if(Arrays.equals(pos, arK)){
 			
-			if(!Arrays.equals(next, arR) && this.knight.MoveChoose(next)){//If statement  checks if arrays are the same and if the inputted next move is valid
+			if(!Arrays.equals(next, arR) && !Arrays.equals(next, arP) && this.knight.MoveChoose(next)){//If statement  checks if arrays are the same and if the inputted next move is valid
 				this.knight.setPos(next);
 				return true;
 
@@ -38,7 +41,7 @@ public abstract class Player{
 		}//end if
 		else if(Arrays.equals(pos, arR)){
 
-			if(!Arrays.equals(next, arK) && this.rook.MoveChoose(next)){//If statement  checks if arrays are the same and if the inputted next move is valid
+			if(!Arrays.equals(next, arK) && !Arrays.equals(next, arP) && this.rook.MoveChoose(next)){//If statement  checks if arrays are the same and if the inputted next move is valid
 				this.rook.setPos(next);
 				return true;				
 
@@ -48,6 +51,21 @@ public abstract class Player{
 			}//end else
 		}//end else if
 		
+		else if(Arrays.equals(pos, arR)){
+
+			if(!Arrays.equals(next, arK) && !Arrays.equals(next, arR) && this.pawn.MoveChoose(next, false)){
+
+				this.rook.setPos(next);
+				return true;
+
+			}//end if
+			else{
+
+				return false;
+			}//end else
+
+
+		}//end else if
 
 		return false;
 	}//end MoveCheck
